74c93f3c5dec8386d6c1d070cd57e517bdac6fda
Prof Flavio <fmotac@gmail.com>
2024-05-31 22:40:08 -0300

    Terceiro

--- app/index.js
+++ app/index.js
@@ -5,5 +5,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+app.use(express.static('public')); 
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
